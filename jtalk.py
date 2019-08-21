3d97dceeee97c749b7e183507eb23d9e99700a66
Ono Yu <ono-yu@ono-yu-2.local>
2019-08-21 16:07:10 +0900

    Modify jtalk.py for mac

--- jtalk.py
+++ jtalk.py
@@ -1,20 +1,19 @@
-#coding: utf-8
 import subprocess
 from datetime import datetime
 
 def jtalk(t):
-    open_jtalk=['open_jtalk']
-    mech=['-x','/var/lib/mecab/dic/open-jtalk/naist-jdic']
-    htsvoice=['-m','/usr/share/hts-voice/mei/mei_normal.htsvoice']
-    speed=['-r','1.0']
-    outwav=['-ow','open_jtalk.wav']
-    cmd=open_jtalk+mech+htsvoice+speed+outwav
-    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
-    c.stdin.write(t.encode())
+    open_jtalk = ['open_jtalk']
+    mech = ['-x', '/usr/local/Cellar/open-jtalk/1.11/dic']
+    htsvoice = ['-m', '/usr/local/Cellar/open-jtalk/1.11/voice/mei/mei_normal.htsvoice']
+    speed = ['-r', '1.0']
+    outwav = ['-ow', 'out.wav']
+    cmd = open_jtalk + mech + htsvoice + speed + outwav
+    c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    c.stdin.write(t.encode('utf-8'))
     c.stdin.close()
     c.wait()
-    paplay = ['paplay','open_jtalk.wav']
-    wr = subprocess.Popen(paplay)
+    aplay = ['afplay', 'out.wav']
+    wr = subprocess.Popen(aplay)
 
 def say_datetime():
     d = datetime.now()
