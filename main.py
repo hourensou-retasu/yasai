cc7dd73f8d1834c3089de0348eaed9af7dd753b3
yasai-nasu2019 <kota-miyaji@c-fo.com>
2019-08-21 14:30:31 +0900

    multi process

--- main.py
+++ main.py
@@ -3,12 +3,24 @@
 from datetime import datetime
 from reserve_dakoku import reserve_dakoku
 from execute_dakoku import execute_dakoku
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Manager, Value, Process
+
 
 def main():
-    executor = ThreadPoolExecutor(max_workers=2)
-    executor.submit(reserve_dakoku)
-    executor.submit(execute_dakoku)
+    with Manager() as manager:
+        RD = reserve_dakoku()
+        ED = execute_dakoku()
+
+        dakoku_queue = manager.list()
+        reserve_process = Process(target=RD.reserve_dakoku, args=[dakoku_queue])
+        observe_process = Process(target=ED.execute_dakoku, args=[dakoku_queue])
+
+        reserve_process.start()
+        observe_process.start()
+
+        reserve_process.join()
+        observe_process.join()
+
 
 if __name__ == "__main__":
     main()
--- reserve_dakoku.py
+++ reserve_dakoku.py
@@ -5,72 +5,85 @@ import re
 import numpy as np
 from jtalk import jtalk
 
-def reserve_dakoku():
+class reserve_dakoku:
+    def __init__(self):
+        self.r = sr.Recognizer()
+        self.mic = sr.Microphone()
 
-    r = sr.Recognizer()
-    mic = sr.Microphone()
+        self.dakoku_patterns = [
+            '.*?(おはよう).*', 
+            '.*?(おつかれ|さき|しつれい|さようなら).*',
+            '.*?(きゅうけい)(はいり|いただきます).*',
+            '.*?(きゅうけい)(あがり|いただきました).*'
+        ]
 
-    dakoku_patterns = [
-        '.*?(おはよう).*', 
-        '.*?(おつかれ|さき|しつれい|さようなら).*',
-        '.*?(きゅうけい)(はいり|いただきます).*',
-        '.*?(きゅうけい)(あがり|いただきました).*'
-    ]
+        self.dakoku_message_dict = {
+            0: 'おはようございます',
+            1: 'おつかれさまでした',
+            2: 'きゅうけいどうぞ',
+            3: 'がんばってください'
+        }
 
-    dakoku_message_dict = {
-        0: 'おはようございます',
-        1: 'おつかれさまでした',
-        2: 'きゅうけいどうぞ',
-        3: 'がんばってください'
-    }
+    def reserve_dakoku(self):
 
-    while True:
+        while True:
+            print("Say something ...")
+
+            with self.mic as source:
+                self.r.adjust_for_ambient_noise(source) #雑音対策
+                audio = self.r.listen(source)
+
+            print ("Now to recognize it...")
+
+            try:
+                recog_text = self.r.recognize_google(audio, language='ja-JP')
+                print(recog_text)
+
+                dakoku_results = [re.match(dakoku_pattern, recog_text) for dakoku_pattern in self.dakoku_patterns]
+            
+                # 打刻WORDにマッチ
+                if any(dakoku_results):
+                    name = face_recog()
+                    
+                    # マッチした打刻種類の最初のindexを取得 0:出勤 1:退勤 2:休憩始 3:休憩終
+                    dakoku_attr = None
+                    for index, dakoku_result in enumerate(dakoku_results):
+                        if dakoku_result is None:
+                            continue
+                        else:
+                            dakoku_attr = index
+                            break
+                            
+                    message = self.dakoku_message_dict[dakoku_attr] + ('、どちらさまですか' if name is None else '、' + name + 'さん')
+                    jtalk(message)
+
+                    if name is None:
+                        name = self.detect_unknown_visitor()
+
+                        # また失敗したとき
+                        if name is None or name not in names:
+                            jtalk('登録されたユーザを認識できませんでした')
+
+                    dakoku_queue.append(name, dakoku_attr)            
+
+            # 以下は認識できなかったときに止まらないように。
+            except sr.UnknownValueError:
+                print("could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    def detect_unknown_visitor(self):
         print("Say something ...")
 
-        with mic as source:
-            r.adjust_for_ambient_noise(source) #雑音対策
-            audio = r.listen(source)
+        with self.mic as source:
+            self.r.adjust_for_ambient_noise(source)  # 雑音対策
+            audio = self.r.listen(source)
 
-        print ("Now to recognize it...")
+        print("Now to recognize it...")
 
         try:
-            recog_text = r.recognize_google(audio, language='ja-JP')
+            recog_text = self.r.recognize_google(audio, language='ja-JP')
             print(recog_text)
-
-            dakoku_results = [re.match(dakoku_pattern, recog_text) for dakoku_pattern in dakoku_patterns]
-        
-            # 打刻WORDにマッチ
-            if any(dakoku_results):
-                name = face_recog()
-
-                # マッチした打刻種類の最初のindexを取得 0:出勤 1:退勤 2:休憩始 3:休憩終
-                
-            # マッチした打刻種類の最初のindexを取得 0:出勤 1:退勤 2:休憩始 3:休憩終
-            dakoku_attr = None
-            for index, dakoku_result in enumerate(dakoku_results):
-                if dakoku_result is None:
-                    continue
-                else:
-                    dakoku_attr = index
-                    break
-            message = dakoku_message_dict[dakoku_attr] + ('、どちらさまですか' if name is None else '、' + name + 'さん')
-            jtalk(message)
-
-            if name is None:
-                name = detect_unknown_visitor()
-
-                # また失敗したとき
-                if name is None or name not in names:
-                    jtalk('登録されたユーザを認識できませんでした')
-
-            dakoku_queue.append(name, dakoku_attr)            
-
-        # 以下は認識できなかったときに止まらないように。
-        except sr.UnknownValueError:
-            print("could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
 
 def main():
     reserve_dakoku()
