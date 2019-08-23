84515f669db72b1741100af9cd570d43cc266be9
hourensou_retasu <54269119+hourensou-retasu@users.noreply.github.com>
2019-08-23 14:13:30 +0900

    refresh token

--- refreshToken.py
+++ refreshToken.py
@@ -4,11 +4,25 @@ import json
 def refreshToken():
   url = "https://accounts.secure.freee.co.jp/public_api/token"
 
-  with open("freeeAPIRefreshToken.json") as f:
+  with open("freeeAPIsecret.json") as f:
     payload = json.load(f)
+
+  payload["grant_type"] = "refresh_token"
+  
+  with open("freeeAPIRefreshToken") as f:
+    payload["refresh_token"] = f.read()
 
   r = requests.post(url, json=payload)
   print(r.text)
+  
+  if r.status_code == 401:
+    return
 
   with open("freeeAPIAccessToken", "w") as f:
-    f.write(r["access_token"])
+    f.write(r.json()["access_token"])
+
+  with open("freeeAPIRefreshToken", "w") as f:
+    f.write(r.json()["refresh_token"])
+
+if __name__ == "__main__":
+  refreshToken()
